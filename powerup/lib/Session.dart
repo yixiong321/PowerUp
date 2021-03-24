7f622f3a4d89fc47952cabe418e9ffe59deebea3
lishengjaw <jawlisheng@gmail.com>
2021-03-24 18:30:12 +0800

    searchcontroller session

--- powerup/lib/Session.dart
+++ powerup/lib/Session.dart
@@ -1,3 +1,5 @@
+
+///////////////////Session/////////////////////////////////
 class Session{
   int _sessionID;
   int _numberOfClasses;
@@ -5,32 +7,39 @@ class Session{
   String _dateTime;
   int _vacancy;
   int _classSize;
-  List<String> _participantList;
+  List<String> participantList;
 
+  //---------Constructors---------//
   Session(this._sessionID, this._numberOfClasses, this._startDate,
-      this._dateTime, this._vacancy, this._classSize, this._participantList);
+      this._dateTime, this._vacancy, this._classSize, this.participantList);
 
-  Map<String, dynamic> toMap(){
-    var map = <String, dynamic>{
-      'sessionID' : _sessionID,
-      'numberOfClasses' : _numberOfClasses,
-      'startDate' : _startDate,
-      'dateTime' : _dateTime,
-      'vacancy' : _vacancy,
-      'classSize' : _classSize,
+  Session.empty();
+
+  //---------Database---------//
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic> {
+      "sessionID":_sessionID,
+      "numberOfClasses":_numberOfClasses,
+      "startDate":_startDate,
+      "dateTime": _dateTime,
+      "vacancy":_vacancy,
+      "classSize": _classSize,
+      //"participantList": participantList; in Register database
     };
     return map;
   }
 
-  Session.fromMap(Map<String, dynamic> map){
+  Session.fromMap(Map<String, dynamic> map) {
     _sessionID = map['sessionID'];
     _numberOfClasses = map['numberOfClasses'];
     _startDate = map['startDate'];
     _dateTime = map['dateTime'];
     _vacancy = map['vacancy'];
     _classSize = map['classSize'];
+    //participantList in Register database
   }
 
+  //---------getters and setters---------//
   int get sessionID => _sessionID;
 
   set sessionID(int value) {
@@ -61,15 +70,26 @@ class Session{
     _vacancy = value;
   }
 
-  int get classSize => _classSize;
+  int get classSize => this._classSize;
 
   set classSize(int value) {
     _classSize = value;
   }
 
-  List<String> get participantList => _participantList;
+  List<String> get participantListTotal => this.participantList; //added Total at the back so that they r of different names from variable
 
-  set participantList(List<String> value) {
-    _participantList = value;
+  set participantListTotal(List<String> participantEmail) {
+    participantList = participantEmail;
   }
+
+  void addToParticipantList(String participantEmail){
+    participantList.add(participantEmail);
+    _vacancy--;
+  }
+
+  void removeFromParticipantList(String participantEmail){
+    participantList.remove(participantEmail);
+    _vacancy++;
+  }
+
 }
